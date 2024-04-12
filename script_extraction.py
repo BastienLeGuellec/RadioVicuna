9e8599abb82c80c6dd65020ca9f83537306fb848
BastienLeGuellec <54061033+BastienLeGuellec@users.noreply.github.com>
2024-04-12 10:48:31 +0200

    Update script_extraction.py

--- script_extraction.py
+++ script_extraction.py
@@ -5,6 +5,9 @@ Uploaded August 2, 2023
 import csv
 from itertools import zip_longest
 
+import shutil
+
+shutil.move("/PATH/inference_radiollm.py", "/PATH/fastchat/serve/inference_radiollm.py")
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
@@ -16,6 +19,9 @@ from rich.live import Live
 
 from fastchat.model.model_adapter import add_model_args
 from fastchat.serve.inference_extraction import ChatIO, chat_loop_extraction
+
+#Do you want to display the logprobs?
+logprobs=True
 
 #Define the model path
 vicuna = '/model_path/'
@@ -64,7 +70,59 @@ class SimpleChatIO(ChatIO):
                 pre = now
         print(" ".join(output_text[pre:]), flush=True)
         return " ".join(output_text)
-chatio = SimpleChatIO()
+
+class SimpleChatIO_log(ChatIO):
+    def __init__(self, multiline: bool = False):
+        self._multiline = multiline
+
+    def prompt_for_input(self, role) -> str:
+        if not self._multiline:
+            return input(f"{role}: ")
+
+        prompt_data = []
+        line = input(f"{role} [ctrl-d/z on empty line to end]: ")
+        while True:
+            prompt_data.append(line.strip())
+            try:
+                line = input()
+            except EOFError as e:
+                break
+        return "\n".join(prompt_data)
+
+    def prompt_for_output(self, role: str):
+        print(f"{role}: ", end="", flush=True)
+
+    def stream_output(self, output_stream):
+        pre = 0
+        log_token=[]
+        log_prob=[]
+        for outputs in output_stream:
+            output_text = outputs["text"]
+            output_log=outputs["logprobs"]
+            
+            output_text = output_text.strip().split(" ")
+            now = len(output_text) - 1
+            if now > pre:
+                print(" ".join(output_text[pre:now]), end=" ", flush=True)
+                pre = now
+        print(" ".join(output_text[pre:]), flush=True)
+        indexlist=[]
+        for i in range(0,len(output_log["tokens"])-1):
+            if output_log["tokens"][i]=="/":
+                indexlist.append(i)
+        for index in indexlist:
+            log_token.append(output_log["tokens"][index+1])
+            log_prob.append(output_log["token_logprobs"][index+1])
+        return " ".join(output_text),[log_token,log_prob]
+
+    def print_output(self, text: str):
+        print(text)
+
+
+if logprobs==True:
+    Chatio_log=SimpleChatIO_log()
+else:
+    chatio = SimpleChatIO()
 
 
 
